6b523947049021c85f6cfa1a52d47876cda2c35b
Lukas Strnad <lukas.strnad222@gmail.com>
2025-06-25 15:19:34 +0200

    added font/ added&routed gamescreen

--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'; // Add this import
 
 @Component({
   selector: 'app-startscreen',
   standalone: true,
   imports: [],
   templateUrl: './startscreen.component.html',
-  styleUrl: './startscreen.component.scss'
+  styleUrls: ['./startscreen.component.scss'] // Use 'styleUrls' (plural)
 })
 export class StartscreenComponent {
-
+  constructor(private router: Router) { }
+  
+  newGame(){
+    this.router.navigate(['/game']);
+  }
 }
