2ecb12b3737522df0330a5b52c2f38c1890f77fe
Lukas Strnad <lukas.strnad222@gmail.com>
2025-08-16 12:41:47 +0200

    added db

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -8,10 +9,24 @@ import { CommonModule } from '@angular/common';
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
+
 export class GameComponent {
   pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game;
+
+  constructor() {
+    this.game = new Game();
+  }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop() || '';
+      this.pickCardAnimation = true;
+      setTimeout(() => {
+        this.game.playedCard.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 800);
+    }
   }
 }
--- src/models/game.ts
+++ src/models/game.ts
@@ -3,4 +3,31 @@ export class Game {
     public stack: string[] = [];
     public playedCard: string[] = [];
     public currentPlayerIndex: number = 0;
+
+    constructor() {
+        for (let i = 1; i < 14; i++) {
+            this.stack.push('ace_' + i);
+            this.stack.push('clubs_' + i);
+            this.stack.push('diamonds_' + i);
+            this.stack.push('hearts_' + i);
+        }
+
+        shuffle(this.stack);
+    }
+}
+
+function shuffle(stack: string[]) {
+  let currentIndex = stack.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [stack[currentIndex], stack[randomIndex]] = [
+      stack[randomIndex], stack[currentIndex]];
+  }
 }
